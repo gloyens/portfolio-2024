8492f866ddb70745c1150c68e95f74352ba2e504
George Gloyens <georgegloyens@gmail.com>
2023-12-10 03:12:29 +0000

    Add CV

--- src/app/pages/CV/index.tsx
+++ src/app/pages/CV/index.tsx
@@ -1,12 +1,32 @@
+import PageWrapper from "@/app/components/PageWrapper";
 import { CVPage } from "./styles";
 
 const CV = () => {
   return (
     <CVPage>
-      <h2>CV</h2>
-      <p>This is my CV page.</p>
+      <PageWrapper>
+        <h2>CV</h2>
+        <p>
+          Below you&apos;ll find my CV. If it&apos;s hard to read, you can
+          either{" "}
+          <a href="/george-gloyens-cv.pdf" download>
+            download it
+          </a>{" "}
+          or{" "}
+          <a href="/george-gloyens-cv.pdf" target="_blank">
+            open it in a new tab
+          </a>
+          .
+        </p>
+      </PageWrapper>
+      <embed
+        src="/george-gloyens-cv.pdf#view=FitH"
+        type="application/pdf"
+        height="600"
+        width="100%"
+      ></embed>
     </CVPage>
-  )
-}
+  );
+};
 
 export default CV;
